9fbb022e45d114e3aa3d6ca8a9184ff4f0aacfd8
hu <you@example.com>
2026-06-08 14:19:57 +0800

    加插件

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,8 +5,18 @@
 mod commands;
 mod eworkhour;
 
+use tauri::Manager;
+
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            // 用户尝试开第二个实例时，把已有窗口拉到前台
+            let _ = app
+                .get_webview_window("main")
+                .expect("no main window")
+                .set_focus();
+        }))
+        .plugin(tauri_plugin_window_state::Builder::default().build())
         .invoke_handler(tauri::generate_handler![
             commands::validate_eteamsid,
             commands::get_projects,
